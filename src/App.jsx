57cd51f2ba30a2442234b37bf922df7ec31a15f5
Josh Shavers <shaversj@gmail.com>
2023-10-29 12:13:39 -0500

    Mobile layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import mark from '/src/assets/images/avatar-mark-webber.webp'
-import { VscCircleFilled } from "react-icons/vsc";
 
 function App() {
 
@@ -7,32 +6,32 @@ function App() {
   return (
     <>
         <div className={"bg-black min-h-screen flex items-center justify-center"}>
-            <main className={"max-w-screen-sm min-h-[900px]  bg-white font-Plus-Jakarta-Sans px-4 py-6"}>
-                <div className={"flex gap-x-2"}>
-                    <h1 className={"text-xl font-bold"}>Notifications</h1>
-                    <div className={"rounded-lg bg-primary-blue w-[32px] h-[28px] text-neutral-white flex items-center justify-center font-bold"}><span>3</span></div>
+            <main className={"max-w-screen-sm min-h-[900px]  bg-white font-Plus-Jakarta-Sans px-4 py-[22.5px]"}>
+                {/*Header*/}
+                <div className={"flex gap-x-2 items-center"}>
+                    <h1 className={"text-[20px] font-extrabold text-very-dark-blue"}>Notifications</h1>
+                    <div className={"rounded-md bg-primary-blue w-[32px] h-[25px] text-light-grayish-blue-1 flex items-center justify-center font-bold"}><span>3</span></div>
                     <button className={"ml-auto"}>
-                        <span className={"text-dark-grayish-blue text-sm "}>Mark all as read</span>
+                        <span className={"text-dark-grayish-blue text-[14.2px] "}>Mark all as read</span>
                     </button>
                 </div>
 
                 {/*Cards Container*/}
-                <div className={"pt-6"}>
+                <div className={"pt-5"}>
 
                     {/*Card*/}
                     <div className={"flex bg-unread-gray p-4 gap-x-[13px] rounded-xl "}>
                         <img className={"w-[40px] h-[40px]"} src={mark} alt={""} />
                         <div className={"text-[14px] text-dark-grayish-blue leading-[1.2rem]"}>
                             <div className={"space-x-2 text-dark-grayish-blue"}>
-                                <span className={"font-extrabold text-black"}>Mark Webber</span>
-                                <span className={"text-dark-grayish-blue"}>reacted to your recent post</span>
+                                <span className={"font-bold text-black"}>Mark Webber</span>
+                                <span className={"text-dark-grayish-blue "}>reacted to your recent post</span>
                                 <span className={"text-dark-grayish-blue font-semibold"}>My first tournament today!</span>
-                                <VscCircleFilled size={"1rem"} style={{display: "inline", color: "hsl(1, 90%, 64%)", verticalAlign: 'text-bottom', marginLeft: "2px"}}/>
+                                <div className={"w-[8px] h-[8px] bg-primary-red rounded-full inline-flex align-middle mb-1"}></div>
                             </div>
                             <span className={"text-grayish-blue"}>1m ago</span>
                         </div>
                     </div>
-
 
                 </div>
 
