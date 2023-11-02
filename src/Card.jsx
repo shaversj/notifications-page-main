6a3ae0125fa79cbdcb594ee457068597897682ae
Josh Shavers <shaversj@gmail.com>
2023-11-02 08:14:42 -0500

    Hide empty elements

--- src/Card.jsx
+++ src/Card.jsx
@@ -16,9 +16,9 @@ const Card = ({notification, decreaseUnReadCount, increaseUnReadCount}) => {
                     <img className={"w-[40px] h-[40px]"} src={notification.avatar} alt={""} />
                     <div className={"text-[14px] text-dark-grayish-blue leading-[1.2rem]"}>
                         <div className={"space-x-2 text-dark-grayish-blue"}>
-                            <span className={"font-bold text-black"}>{notification.name}</span>
-                            <span className={"text-dark-grayish-blue "}>{notification.message}</span>
-                            <span className={"text-dark-grayish-blue font-semibold"}>{notification.message_location}</span>
+                            <span className={"font-bold text-black empty:hidden"}>{notification.name}</span>
+                            <span className={"text-dark-grayish-blue empty:hidden"}>{notification.message}</span>
+                            <span className={"text-dark-grayish-blue font-semibold empty:hidden"}>{notification.message_location}</span>
                         </div>
                         <span className={"text-grayish-blue"}>{notification.timestamp}</span>
                     </div>
@@ -29,9 +29,9 @@ const Card = ({notification, decreaseUnReadCount, increaseUnReadCount}) => {
                         <img className={"w-[40px] h-[40px]"} src={notification.avatar} alt={""} />
                         <div className={"text-[14px] text-dark-grayish-blue leading-[1.2rem]"}>
                             <div className={"space-x-2 text-dark-grayish-blue"}>
-                                <span className={"font-bold text-black"}>{notification.name}</span>
-                                <span className={"text-dark-grayish-blue "}>{notification.message}</span>
-                                <span className={"text-dark-grayish-blue font-semibold"}>{notification.message_location}</span>
+                                <span className={"font-bold text-black empty:hidden"}>{notification.name}</span>
+                                <span className={"text-dark-grayish-blue empty:hidden"}>{notification.message}</span>
+                                <span className={"text-dark-grayish-blue font-semibold empty:hidden"}>{notification.message_location}</span>
                                 <div className={"w-[8px] h-[8px] bg-primary-red rounded-full inline-flex align-middle mb-1"}></div>
                             </div>
                             <span className={"text-grayish-blue"}>{notification.timestamp}</span>
