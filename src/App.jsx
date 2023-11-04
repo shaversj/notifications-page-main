b1b0ae67d2ef3ead4f48a641a1db59e615fc55a7
Josh Shavers <shaversj@gmail.com>
2023-11-04 11:33:46 -0500

    Add prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,51 @@
-import CardLayout from "./CardLayout.jsx";
-import {useState} from "react";
-import notificationData from './data.json'
+import { useState } from "react";
+import notificationData from "./data.json";
+import Card from "./Card.jsx";
 
 function App() {
-    const [notifications, setNotifications] = useState(notificationData)
-    const [unReadCount, setUnReadCount] = useState(notifications.filter(notification => notification.hasBeenRead === false).length)
+  const [notifications, setNotifications] = useState(notificationData);
+  const [unReadCount, setUnReadCount] = useState(notifications.filter((notification) => notification.hasBeenRead === false).length);
 
-    function markAllAsRead(){
-        const newNotifications = notifications.map(notification => {
-            return {...notification, hasBeenRead: true}
-        })
-        setNotifications(newNotifications)
-        setUnReadCount(0)
-    }
-    function increaseUnReadCount(){
-        setUnReadCount(unReadCount + 1)
-    }
+  function markAllAsRead() {
+    const newNotifications = notifications.map((notification) => {
+      return { ...notification, hasBeenRead: true };
+    });
+    setNotifications(newNotifications);
+    setUnReadCount(0);
+  }
 
-    function decreaseUnReadCount(){
-        setUnReadCount(unReadCount - 1)
-    }
+  function increaseUnReadCount() {
+    setUnReadCount(unReadCount + 1);
+  }
 
+  function decreaseUnReadCount() {
+    setUnReadCount(unReadCount - 1);
+  }
 
   return (
     <>
-        <div className={"bg-black min-h-screen flex items-center justify-center"}>
-            <div className={"lg:w-full lg:max-w-screen-lg lg:min-h-[990px] lg:bg-lg-background-color lg:flex lg:items-center"}>
-                <main className={"max-w-screen-sm  lg:min-h-0 lg:mx-auto lg:max-w-[732px] bg-white font-Plus-Jakarta-Sans px-4 py-[22.5px] lg:py-0 lg:pt-8 lg:pb-7 lg:px-8 lg:rounded-2xl shadow"}>
-                    {/*Header*/}
-                    <div className={"flex gap-x-2 items-center"}>
-                        <h1 className={"text-[20px] lg:text-[24px] lg:font-bold font-extrabold text-very-dark-blue"}>Notifications</h1>
-                        <div className={"rounded-md bg-primary-blue w-[32px] h-[25px] lg:ml-1 text-light-grayish-blue-1 flex items-center justify-center font-bold"}><span>{unReadCount}</span></div>
-                        <button className={"ml-auto"} onClick={markAllAsRead}>
-                            <span className={"text-dark-grayish-blue text-[14.2px] lg:text-[16.2px]"}>Mark all as read</span>
-                        </button>
-                    </div>
-
-                    <CardLayout notifications={notifications} increaseUnReadCount={increaseUnReadCount} decreaseUnReadCount={decreaseUnReadCount} />
-
-
-                </main>
+      <div className={"bg-black min-h-screen flex items-center justify-center"}>
+        <div className={"lg:w-full lg:max-w-screen-lg lg:min-h-[990px] lg:bg-lg-background-color lg:flex lg:items-center"}>
+          <main className={"max-w-screen-sm lg:mx-auto lg:max-w-[732px] bg-white font-Plus-Jakarta-Sans px-4 py-[22.5px] lg:py-0 lg:px-8 lg:rounded-2xl shadow-sm"}>
+            <div className={"flex gap-x-2 items-center lg:pt-7"}>
+              <h1 className={"text-[20px] lg:text-[24px] lg:font-bold font-extrabold text-very-dark-blue"}>Notifications</h1>
+              <div className={"rounded-md bg-primary-blue w-[32px] h-[25px] lg:ml-1 text-light-grayish-blue-1 flex items-center justify-center font-bold"}>
+                <span>{unReadCount}</span>
+              </div>
+              <button className={"ml-auto"} onClick={markAllAsRead}>
+                <span className={"text-dark-grayish-blue text-[14.2px] lg:text-[16.2px] hover:text-primary-blue"}>Mark all as read</span>
+              </button>
             </div>
 
-
+            <div className={"pt-[20px] lg:pt-[29px] space-y-[10px]"}>
+              {notifications &&
+                notifications.map((notification) => <Card key={notification.id} notification={notification} increaseUnReadCount={increaseUnReadCount} decreaseUnReadCount={decreaseUnReadCount} />)}
+            </div>
+          </main>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,52 +1,47 @@
-const Card = ({notification, decreaseUnReadCount, increaseUnReadCount}) => {
-    function toggleUnRead(){
-        notification.hasBeenRead = false
-        increaseUnReadCount()
-    }
+const Card = ({ notification, decreaseUnReadCount, increaseUnReadCount }) => {
+  function toggleUnRead() {
+    notification.hasBeenRead = false;
+    increaseUnReadCount();
+  }
 
-    function toggleRead(){
-        notification.hasBeenRead = true
-        decreaseUnReadCount()
-    }
+  function toggleRead() {
+    notification.hasBeenRead = true;
+    decreaseUnReadCount();
+  }
 
-    return (
-        <>
-            {notification.hasBeenRead ? (
-                <button className={"flex bg-neutral-white p-4 gap-x-[13px] rounded-md w-full text-left lg:gap-x-[19px] lg:px-5 lg:py-4"} onClick={toggleUnRead}>
-                    <img className={"w-[40px] h-[40px] lg:w-[45px] lg:h-[45px]"} src={notification.avatar} alt={""} />
-                    <div className={"text-[14px] lg:text-[16px] text-dark-grayish-blue leading-[1.2rem] lg:leading-[1.5rem]"}>
-                        <div className={"space-x-2 text-dark-grayish-blue"}>
-                            <span className={"font-bold text-black empty:hidden hover:text-primary-blue"}>{notification.name}</span>
-                            <span className={"text-dark-grayish-blue empty:hidden"}>{notification.message}</span>
-                            <span className={"text-dark-grayish-blue font-semibold empty:hidden hover:text-primary-blue lg:font-bold"}>{notification.message_location}</span>
-                        </div>
-                        <span className={"text-grayish-blue"}>{notification.timestamp}</span>
-                        <div className={"empty:hidden border rounded-md px-5 py-3 mt-2 lg:py-[20px] leading-5 lg:text-[16.3px]"}>
-                            {notification.reply}
-                        </div>
-                    </div>
-                    <img className={"w-[39px] h-[39px] lg:w-[45px] lg:h-[47px] ml-auto [&:not([src])]:hidden"} src={notification.commented_picture} alt={""} />
-                </button>
-            ):
-                (
-                    <button className={"flex bg-unread-gray p-4 gap-x-[13px] rounded-md w-full text-left lg:gap-x-[19px] lg:px-5 lg:py-4"} onClick={toggleRead}>
-                        <img className={"w-[40px] h-[40px] lg:w-[45px] lg:h-[45px] "} src={notification.avatar} alt={""} />
-                        <div className={"text-[14px] lg:text-[16.15px] text-dark-grayish-blue leading-[1.2rem] lg:leading-[1.5rem]"}>
-                            <div className={"space-x-2 text-dark-grayish-blue"}>
-                                <span className={"font-bold text-black empty:hidden hover:text-primary-blue"}>{notification.name}</span>
-                                <span className={"text-dark-grayish-blue empty:hidden"}>{notification.message}</span>
-                                <span className={"text-dark-grayish-blue font-semibold empty:hidden hover:text-primary-blue lg:font-bold"}>{notification.message_location}</span>
-                                <div className={"w-[8px] h-[8px] bg-primary-red rounded-full inline-flex align-middle mb-1"}></div>
-                            </div>
-                            <span className={"text-grayish-blue"}>{notification.timestamp}</span>
-                        </div>
-                        <img className={"w-[40px] h-[40px] lg:w-[45px] lg:h-[47px] ml-auto [&:not([src])]:hidden"} src={notification.commented_picture} alt={""} />
-                    </button>
-            )}
-        </>
-
-
-    );
+  return (
+    <>
+      {notification.hasBeenRead ? (
+        <button className={"flex bg-neutral-white p-4 gap-x-[13px] w-full text-left lg:gap-x-[19px] lg:px-5 lg:py-4"} onClick={toggleUnRead}>
+          <img className={"w-[40px] h-[40px] lg:w-[45px] lg:h-[45px]"} src={notification.avatar} alt={""} />
+          <div className={"text-[14px] lg:text-[16px] text-dark-grayish-blue leading-[1.2rem] lg:leading-[1.5rem]"}>
+            <div className={"space-x-2 text-dark-grayish-blue"}>
+              <span className={"font-bold text-black empty:hidden hover:text-primary-blue"}>{notification.name}</span>
+              <span className={"text-dark-grayish-blue empty:hidden"}>{notification.message}</span>
+              <span className={"text-dark-grayish-blue font-semibold empty:hidden hover:text-primary-blue lg:font-bold"}>{notification.message_location}</span>
+            </div>
+            <span className={"text-grayish-blue"}>{notification.timestamp}</span>
+            <div className={"empty:hidden border rounded-md px-5 py-3 mt-2 lg:py-[20px] leading-5 lg:text-[16.3px]"}>{notification.reply}</div>
+          </div>
+          <img className={"w-[39px] h-[39px] lg:w-[45px] lg:h-[47px] ml-auto [&:not([src])]:hidden"} src={notification.commented_picture} alt={""} />
+        </button>
+      ) : (
+        <button className={"flex bg-unread-gray p-4 gap-x-[13px] rounded-lg w-full text-left lg:gap-x-[19px] lg:px-5 lg:py-4 shadow-sm"} onClick={toggleRead}>
+          <img className={"w-[40px] h-[40px] lg:w-[45px] lg:h-[45px] "} src={notification.avatar} alt={""} />
+          <div className={"text-[14px] lg:text-[16.15px] text-dark-grayish-blue leading-[1.2rem] lg:leading-[1.5rem]"}>
+            <div className={"space-x-2 text-dark-grayish-blue"}>
+              <span className={"font-bold text-black empty:hidden hover:text-primary-blue"}>{notification.name}</span>
+              <span className={"text-dark-grayish-blue empty:hidden"}>{notification.message}</span>
+              <span className={"text-dark-grayish-blue font-semibold empty:hidden hover:text-primary-blue lg:font-bold"}>{notification.message_location}</span>
+              <div className={"w-[8px] h-[8px] bg-primary-red rounded-full inline-flex align-middle mb-1"}></div>
+            </div>
+            <span className={"text-grayish-blue"}>{notification.timestamp}</span>
+          </div>
+          <img className={"w-[40px] h-[40px] lg:w-[45px] lg:h-[47px] ml-auto [&:not([src])]:hidden"} src={notification.commented_picture} alt={""} />
+        </button>
+      )}
+    </>
+  );
 };
 
 export default Card;
